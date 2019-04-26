140df745164c9f4dee14c7ded4c787d33a8cab5c
lkmatos <leonardokmatos@gmail.com>
2019-04-26 17:27:39 -0300

    firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,6 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyDM5Pa5WlLUgLN7mpi4zOBgseuSHd9qZI8',
-    authDomain: 'ionicfirestoretasks.firebaseapp.com',
-    databaseURL: 'https://ionicfirestoretasks.firebaseio.com',
-    projectId: 'ionicfirestoretasks',
-    storageBucket: 'ionicfirestoretasks.appspot.com',
-    messagingSenderId: '700329941828'
   }
 };
 
