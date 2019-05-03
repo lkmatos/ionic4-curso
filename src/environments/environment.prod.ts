641ac76ae696c4c1c874d79fd6e4ae598c445f4b
lkmatos <leonardokmatos@gmail.com>
2019-05-03 11:19:04 -0300

    conclusão do curso

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: true,
   firebaseConfig: {
+    apiKey: 'AIzaSyDM5Pa5WlLUgLN7mpi4zOBgseuSHd9qZI8',
+    authDomain: 'ionicfirestoretasks.firebaseapp.com',
+    databaseURL: 'https://ionicfirestoretasks.firebaseio.com',
+    projectId: 'ionicfirestoretasks',
+    storageBucket: 'ionicfirestoretasks.appspot.com',
+    messagingSenderId: '700329941828'
   }
 };
